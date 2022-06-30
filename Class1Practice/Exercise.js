43ecd4a2dd0d9a281dfba629705f56ce0f1b02cb
Raveel <raveelch@gmail.com>
2022-06-29 21:43:01 -0400

    Work

---
+++ Class1Practice/Exercise.js
@@ -0,0 +1,39 @@
+/**
+ * 1. Create a variable name accountBalance and assign 1000 to that variable
+2. Lets assume you spent $1 at the park when you went our for walk
+3. Post Decrement a dollar from you accountBalance
+4. Construct a string which should display below text
+
+I spent a dollar to buy a water and not my new balance is N
+
+1. Create a variable name age and assign your age to that variable
+2. Lets assume today is your birthday 
+3. Post Increment your age
+4. Construct a string which should display below text
+
+Since today is my birthday, I am N years of old
+
+ */
+
+let accountBalance = 1000;
+accountBalance--;
+
+console.log (`\n\nI spent a dollar to buy water, and now my new balance is ${accountBalance}.`);
+
+let myAge = 24;
+myAge++;
+
+console.log (`\n\nSince today is my birthday, I am ${myAge} years old.`);
+
+numberOfPeople = 1;
+
+console.log (`\n\nThe marathon log is as follows.`);
+console.log (`At 2, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:05, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:12, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:27, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:30, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:38, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:45, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:55, total of ${numberOfPeople++} people crossed the line. `);
+console.log (`At 2:57, total of ${numberOfPeople++} people crossed the line. `);
